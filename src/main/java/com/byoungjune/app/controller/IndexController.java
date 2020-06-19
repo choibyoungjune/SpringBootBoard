e93ac85aaa79d927f804a79ce23559a45138d1b8
Choi <66340382+choibyoungjune@users.noreply.github.com>
2020-06-19 17:33:40 +0900

    게시판 만들기(10개씩 끊어서 넘김) (#3)

--- src/main/java/com/byoungjune/app/controller/IndexController.java
+++ src/main/java/com/byoungjune/app/controller/IndexController.java
@@ -1,30 +1,12 @@
 package com.byoungjune.app.controller;
 
-import com.byoungjune.app.domain.Board;
-import com.byoungjune.app.repository.BoardRepository;
-import com.byoungjune.app.repository.UserRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
-    private BoardRepository boardRepository;
-    private UserRepository userRepository;
-
-    public IndexController(BoardRepository boardRepository, UserRepository userRepository) {
-        this.boardRepository = boardRepository;
-        this.userRepository = userRepository;
-    }
-
     @GetMapping("/")
-    public String index(Model model) {
-        List<Board> boardList = boardRepository.findAll();
-        model.addAttribute("boardList", boardList);
+    public String index() {
         return "index";
     }
 }
